898485886ffd1d2d84a2e23cfea7b9f7a3207b3a
faizaan <webfaizaan@gmail.com>
2021-10-23 22:45:13 +0530

    implemented saving user in db

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,31 @@
+const bcrypt = require("bcryptjs");
+
 const validate = require("../validations/auth");
+const User = require("../models/user");
 
 const register = async (req, res, next) => {
   try {
     const value = await validate.register(req.body);
 
-    res.json({ message: "Success" });
+    const { name, email, password } = value;
+
+    const existingEmail = await User.findOne({ email });
+
+    if (existingEmail) {
+      const error = new Error("Email already exists");
+      error.status = 400;
+      return next(error);
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const savedUser = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({ success: true, user: savedUser._id });
   } catch (error) {
     next(error);
   }
